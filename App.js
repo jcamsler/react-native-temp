06fc64303646abc3b80d9cb28ca7e9f54b7652cc
Jc AMSLER <jcamsler@gmail.com>
2024-07-11 17:33:22 +0200

    Step 2

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { s } from "./App.style";
 
 export default function App() {
-  return <Text>Hello JC ! </Text>;
+  return (
+    <View style={s.container}>
+      <View style={s.workspace}>
+        <View>
+          <Text>Temperature</Text>
+        </View>
+        <View>
+          <Text>Input</Text>
+        </View>
+        <View>
+          <Text>Button</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
---
+++ App.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center" },
+  workspace: {
+    height: 450,
+    backgroundColor: "red",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+});
+
+export { s };
