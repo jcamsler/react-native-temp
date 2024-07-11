163141e91e81e6f3044228a1fa3a211d8206dd7e
Jc AMSLER <jcamsler@gmail.com>
2024-07-11 18:19:21 +0200

    3e commit, ajout couleur de fond et champ texte

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
+import hotBackground from "./assets/hot.png";
+import { InputTemperature } from "./components/InputTemperature/InputTemperature";
 
 export default function App() {
   return (
-    <View style={s.container}>
+    <ImageBackground style={s.container} source={hotBackground}>
       <View style={s.workspace}>
         <View>
           <Text>Temperature</Text>
         </View>
-        <View>
-          <Text>Input</Text>
-        </View>
+        <InputTemperature defaultValue={"12"} />
         <View>
           <Text>Button</Text>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
--- App.style.js
+++ App.style.js
@@ -1,10 +1,9 @@
 import { StyleSheet } from "react-native";
 
 const s = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center" },
+  container: { flex: 1, justifyContent: "center", padding: 20 },
   workspace: {
     height: 450,
-    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "space-evenly",
   },
